99cc37f5a8462d8ea399d2ef440c00a8c0c4adfe
Dan F-M <danfm@nyu.edu>
2016-06-24 13:15:32 -0400

    different batch size

--- train.py
+++ train.py
@@ -4,6 +4,7 @@
 from __future__ import division, print_function
 
 import os
+import glob
 import json
 import codecs
 import argparse
@@ -16,11 +17,25 @@ from keras.layers import Dense, Activation, Dropout
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--output-dir", default="output",
                     help="the output directory")
+parser.add_argument("-w", "--with-will", action="store_true",
+                    help="y'know")
 args = parser.parse_args()
 outdir = args.output_dir
 
 with codecs.open("trump-speeches/speeches.txt", "r", "utf-8") as f:
     lines = [line.strip() for line in f if len(line.strip())]
+
+if args.with_will:
+    count0 = len(lines)
+    for fn in glob.glob("../Fakespeare/plays/*.txt"):
+        with open(fn, "r") as f:
+            txt = f.read()
+            lines += [line.strip() for line in txt.splitlines()
+                      if ("*" not in line and "_" not in line and
+                          "<" not in line and len(line.strip()))]
+            if len(lines) > 3*count0:
+                break
+
 text = " ".join(lines)
 text = text.replace("–", "-")
 text = text.replace("—", "-")
@@ -33,8 +48,8 @@ indices_char = dict((i, c) for i, c in enumerate(chars))
 
 # cut the text in semi-redundant sequences of maxlen characters
 maxlen = 50
-step = 3
-batch_size = 128
+step = 2
+batch_size = 36
 sentences = []
 next_chars = []
 for i in range(0, len(text) - maxlen, step):
@@ -54,10 +69,10 @@ for i, sentence in enumerate(sentences):
 # build the model: 2 stacked LSTM
 print('Build model...')
 model = Sequential()
-model.add(LSTM(512, return_sequences=True,
+model.add(LSTM(256, return_sequences=True,
                input_shape=(maxlen, len(chars))))
 model.add(Dropout(0.2))
-model.add(LSTM(512, return_sequences=False, ))
+model.add(LSTM(256, return_sequences=False, ))
 model.add(Dropout(0.2))
 model.add(Dense(len(chars)))
 model.add(Activation("softmax"))
@@ -84,7 +99,7 @@ for iteration in range(1, 60):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y, batch_size=batch_size, nb_epoch=1)
+    model.fit(X, y, batch_size=batch_size, nb_epoch=1, shuffle=True)
 
     model.save_weights(os.path.join(outdir,
                                     'weights_{0:05d}.h5'.format(iteration)))
